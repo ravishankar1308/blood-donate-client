a3ab77a9504d71eb5a53fb0bf5d46d2b1b4da567
ravishankar1308 <ravishankar.0813@gmail.com>
2020-05-21 02:31:49 +0530

    all user list

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -39,6 +39,8 @@ import DonarDetail from '../screens/driver/DonarDetail';
 import AllUser from '../screens/admin/AllUser';
 import Driverlist from '../screens/admin/Driverlist';
 import UserDetail from '../screens/admin/UserDetail';
+import DriverList from '../screens/admin/Driverlist';
+import AddDriver from '../screens/admin/AddDriver';
 
 import AuthLoading from '../screens/auth/AuthLoading';
 
@@ -49,20 +51,20 @@ import {View, Text} from 'react-native';
 const iconSize = 24;
 
 const appNavigator = createStackNavigator(
-  {
-    Index: IndexScreen,
-      Show: ShowScreen,
-      Create: CreateScreen,
-      Edit: EditScreen,
-      BookIndex: BookIndexScreen,
-      BookShow: BookShowScreen,
-      BookCreate: BookCreateScreen,
-      BookEdit: BookEditScreen,
-      SignIn: SignInScreen,
-      SignUp: SignUpScreen,
-      Loading: AuthLoading,
-      Resolve: ResolveAuthScreen,
-  },
+    {
+        Index: IndexScreen,
+        Show: ShowScreen,
+        Create: CreateScreen,
+        Edit: EditScreen,
+        BookIndex: BookIndexScreen,
+        BookShow: BookShowScreen,
+        BookCreate: BookCreateScreen,
+        BookEdit: BookEditScreen,
+        SignIn: SignInScreen,
+        SignUp: SignUpScreen,
+        Loading: AuthLoading,
+        Resolve: ResolveAuthScreen,
+    },
     // {
     //   initialRouteName: 'Resolve',
     //   defaultNavigationOptions: {
@@ -74,6 +76,16 @@ const appNavigator = createStackNavigator(
 const AccidentInfo = createStackNavigator({
     AllAccident: {screen: AllAccident},
     AccidentDetail: {screen: AccidentDetail},
+});
+
+const userInfo = createStackNavigator({
+    AllUser: {screen: AllUser},
+    UserDetail: {screen: UserDetail},
+});
+const driverInfo = createStackNavigator({
+    DriverList: {screen: DriverList},
+    UserDetail: {screen: UserDetail},
+    AddDriver: {screen: AddDriver},
 });
 
 const switchNavigator = createSwitchNavigator(
@@ -95,16 +107,16 @@ const switchNavigator = createSwitchNavigator(
                             <Icon name="home" color={tintColor} size={24}/>
                         ),
                     },
-        },
-        AddRequest: {
-          screen: AddRequest,
-          navigationOptions: {
-            tabBarLabel: 'Request',
-            tabBarIcon: ({tintColor}) => (
-              <Ionicons
-                name="md-add-circle-outline"
-                color={tintColor}
-                size={iconSize}
+                },
+                AddRequest: {
+                    screen: AddRequest,
+                    navigationOptions: {
+                        tabBarLabel: 'Request',
+                        tabBarIcon: ({tintColor}) => (
+                            <Ionicons
+                                name="md-add-circle-outline"
+                                color={tintColor}
+                                size={iconSize}
               />
             ),
           },
@@ -136,7 +148,7 @@ const switchNavigator = createSwitchNavigator(
             navigationOptions: {
                 tabBarLabel: 'Accident',
                 tabBarIcon: ({tintColor}) => (
-                    <MaterialIcons
+                    <FontAwesome5
                         name="event-seat"
                         color={tintColor}
                         size={iconSize}
@@ -153,24 +165,24 @@ const switchNavigator = createSwitchNavigator(
                 ),
             },
         },
-      AccountInfo: {
-        screen: AccountInfo,
-        navigationOptions: {
-          tabBarLabel: 'Account',
-            tabBarIcon: ({tintColor}) => (
-                <MaterialCommunityIcons
-                    name="account"
-                    color={tintColor}
-                    size={iconSize}
-                />
-            ),
+        AccountInfo: {
+            screen: AccountInfo,
+            navigationOptions: {
+                tabBarLabel: 'Account',
+                tabBarIcon: ({tintColor}) => (
+                    <MaterialCommunityIcons
+                        name="account"
+                        color={tintColor}
+                        size={iconSize}
+                    />
+                ),
+            },
         },
-      },
     }),
         adminFlow: createMaterialBottomTabNavigator(
             {
                 AllUser: {
-                    screen: AllUser,
+                    screen: userInfo,
                     navigationOptions: {
                         tabBarLabel: 'User',
                         tabBarIcon: ({tintColor}) => (
@@ -178,13 +190,13 @@ const switchNavigator = createSwitchNavigator(
                         ),
                     },
                 },
-                AddRequest: {
-                    screen: AddRequest,
+                DriverList: {
+                    screen: driverInfo,
                     navigationOptions: {
-                        tabBarLabel: 'Request',
+                        tabBarLabel: 'Driver',
                         tabBarIcon: ({tintColor}) => (
-                            <Ionicons
-                                name="md-add-circle-outline"
+                            <FontAwesome5
+                                name="user-astronaut"
                                 color={tintColor}
                                 size={iconSize}
                             />
@@ -222,14 +234,14 @@ const switchNavigator = createSwitchNavigator(
                 BookCreate: BookCreateScreen,
                 Account: AccountScreen,
             },
-      // {
-      //   initialRouteName: 'bookFlow',
-      //   activeColor: '#f0edf6',
-      //   inactiveColor: '#3e2465',
-      //   barStyle: {backgroundColor: '#694fad'},
-      // },
-    ),
-  },
+            // {
+            //   initialRouteName: 'bookFlow',
+            //   activeColor: '#f0edf6',
+            //   inactiveColor: '#3e2465',
+            //   barStyle: {backgroundColor: '#694fad'},
+            // },
+        ),
+    },
   {
     // initialRouteName: 'loginFlow',
     defaultNavigationOptions: {
